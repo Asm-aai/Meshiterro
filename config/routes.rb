fb5910b633756a4278aff030ccfe7ddfc707ae0e
EC2 Default User <ec2-user@ip-172-31-34-19.ap-northeast-3.compute.internal>
2024-08-16 05:10:57 +0000

    [finish]4-10

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :post_images, only: [:new, :show, :index]
+  resources :post_images, only: [:new, :create, :show, :index]
   get 'post_image/new'
   get 'post_image/show'
   get 'post_image/index'
